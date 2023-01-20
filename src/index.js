1ae7c0a1c2eb76bb91f06ec397946a77b3c8fa5f
Rafis Isyanov <rr.isyanov@gmail.com>
2023-01-20 09:50:20 +0500

    feat: do reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let result = '';
+
+  for (let i = String(Math.abs(n)).length - 1; i >= 0; i = i - 1) {
+    result = result + String(Math.abs(n))[i];
+  }
+
+  return result;
 }
